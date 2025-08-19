a16449aea4a28cd748950563f8bb807181e683aa
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2025-08-19 15:31:50 +0600

    Initial commit

---
+++ src/main/java/iut/cse/trade_square_user_auth/TradeSquareUserAuthApplication.java
@@ -0,0 +1,13 @@
+package iut.cse.trade_square_user_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSquareUserAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeSquareUserAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iut/cse/trade_square_user_auth/TradeSquareUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package iut.cse.trade_square_user_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSquareUserAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
